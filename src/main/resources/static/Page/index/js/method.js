47fb134f9e2a8ef1dd209bf80588d5bd4b9a5e56
anyongliang <anyongliang@icreations.com.cn>
2019-06-21 10:48:44 +0800

    修改：写入评论加判断逻辑

--- src/main/resources/static/Page/index/js/method.js
+++ src/main/resources/static/Page/index/js/method.js
@@ -3,12 +3,23 @@ function writeComment() {
     var commentEmail = document.getElementById("commentEmail").value;
     var commentName = document.getElementById("commentName").value;
     var commentInfo = document.getElementById("commentInfo").value;
-    $.ajax({
-        type: "post",
-        url: "/CommentService/WriteComment",
-        data: {commentInfo: commentInfo, commentName: commentName, commentEmail: commentEmail},
-        success: function (data) {
-            alert(data.message)
+    //昵称判空
+    if (commentName.length == 0) {
+        alert("昵称不能为空!")
+    } else {
+        //content判空
+        if (commentInfo.length == 0) {
+            alert("请随便写点什么在提交OVO.")
+        } else {
+            //post请求写评论
+            $.ajax({
+                type: "post",
+                url: "/CommentService/WriteComment",
+                data: { commentInfo: commentInfo, commentName: commentName, commentEmail: commentEmail },
+                success: function (data) {
+                    alert(data.message)
+                }
+            });
         }
-    });
+    }
 }
