c8f54430914b38d8797535c7b20c0b15a0e2834d
anyongliang <anyongliang@icreations.com.cn>
2019-06-27 15:59:32 +0800

    修改：完善资源夹

--- src/main/java/cn/anyongliang/config/Const.java
+++ src/main/java/cn/anyongliang/config/Const.java
@@ -7,30 +7,29 @@ package cn.anyongliang.config;
 public class Const {
 
     /**
-     * Redis-pool
-     * Redisson-pool
+     * 通用connect配置
      */
-    public static String RedisHost = "127.0.0.1";
+    public static String publishHost = "127.0.0.1";
+
+    /* Redis-pool,Redisson-pool */
+    public static String RedisHost = publishHost;
     public static int RedisPort = 6379;
     public static int RedisTimeOut = 10000;
     public static String RedisAuth = "";
     public static int RedisDatabase = 0;
 
-    /**
-     * jdbc-mysql/MariaDB
-     */
+    /*jdbc-mysql/MariaDB*/
     public static String JdbcDriver = "com.mysql.cj.jdbc.Driver";
-    public static String JdbcHost = "127.0.0.1";
+    public static String JdbcHost = publishHost;
     public static String JdbcPort = "3306";
     public static String JdbcUser = "root";
     public static String JdbcDBName = "blog";
     public static String JdbcPassword = "123456";
     public static String SPACE = " ";
 
-    /**
-     * mongoDB
-     */
-    public static String MongoHost = "16.16.11.1:27017";
+    /*mongoDB*/
+    public static String MongoHost = publishHost;
+    public static String MongoPort = "27017";
     public static String MongoDBName = "file";
 
 }
